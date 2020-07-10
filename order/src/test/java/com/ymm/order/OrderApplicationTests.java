d54d43327d8fecda2c35e9c3082838e921d0b23f
余梦满 <754865517@qq.com>
2020-07-10 17:11:00 +0800

    第一次提交。

---
+++ order/src/test/java/com/ymm/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ymm.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
